65078a90f47d5b6a340876e4cbd1b8bebce357dd
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-06-15 10:40:27 +0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+README = read('README.rst')
+
+
+setup(
+    name = "django-coffeescript",
+    packages = find_packages(),
+    version = "0.1",
+    author = "Andrey Fedoseev",
+    author_email = "andrey.fedoseev@gmail.com",
+    url = "https://github.com/andreyfedoseev/django-coffeescript",
+    description = "Django template tags to compile CoffeeScript",
+    long_description = README,
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],    
+    keywords = ["coffeescript"],
+)
